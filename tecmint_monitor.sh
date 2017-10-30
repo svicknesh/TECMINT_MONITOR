1daeff58b77e4131907ccecf1f5610c3d063b046
Vicknesh Suppramaniam <vicknesh.suppramaniam@gmail.com>
2017-10-30 11:21:18 +0800

    Disk Usage is obtained from lsblk instead of being hardcoded. Useful for VMs.

--- tecmint_monitor.sh
+++ tecmint_monitor.sh
@@ -137,7 +137,8 @@ echo -e '\E[32m'"Swap Usages :" $tecreset
 cat /tmp/ramcache | grep -v "Mem"
 
 # Check Disk Usages
-df -h| grep 'Filesystem\|/dev/sda*' > /tmp/diskusage
+hddisk=$(lsblk | grep disk | awk '{print $1}')
+df -h| grep "Filesystem\|/dev/${hddisk}*" > /tmp/diskusage
 echo -e '\E[32m'"Disk Usages :" $tecreset 
 cat /tmp/diskusage
 
